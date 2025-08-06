6a61be4a7a9d5ef585b6a2a357fb43b993092f8b
Hirsep <peter@hirsnik.at>
2025-08-06 13:33:12 +0200

    v0.1

--- household/settings.py
+++ household/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-za4kh9*+oz1@7#g7g851dgd@ongqj2n6x(^#4$z$)8p9_0a5cs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
